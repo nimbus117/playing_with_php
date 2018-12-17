2510e173feba36c2ded9461c6721f8108a3eec18
nimbus117 <mail@nimbus117.co.uk>
2018-12-17 19:56:11 +0000

    added rock paper scissors

---
+++ rps.php
@@ -0,0 +1,33 @@
+#!/usr/bin/php
+<?php
+
+/**
+ * Class Game
+ * @author nimbus117
+ */
+class Game {
+
+  private $player1;
+  private $player2;
+  private $whoBeatsWho = [
+    'rock' => 'scissors',
+    'paper' => 'rock',
+    'scissors' => 'paper'
+  ];
+
+  public function __construct($player1, $player2) {
+    $this->player1 = strtolower($player1);
+    $this->player2 = strtolower($player2);
+  }
+
+  public function getWinner() {
+
+    if ($this->player1 == $this->player2) return 'draw';
+    return $this->whoBeatsWho[$this->player1] == $this->player2
+      ? 'player1 wins'
+      : 'player2 wins';
+  }
+}
+
+$game = new Game($argv[1], $argv[2]);
+echo $game->getWinner();
